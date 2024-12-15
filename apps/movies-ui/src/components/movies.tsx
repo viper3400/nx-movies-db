92d12b65466e7466458c3767540921e6b54cd785
viper3400 <viper_3400@gmx.de>
2024-12-15 11:52:33 +0100

    Fix search text validation

--- apps/movies-ui/src/components/movies.tsx
+++ apps/movies-ui/src/components/movies.tsx
@@ -23,12 +23,15 @@ export const MovieComponent: React.FC = () => {
     console.log(result);
     setSearchResult(result);
   };
+
+  const validateSearch = (text: string) => {
+    setInvalidSearch(text.length < 3);
+  };
+
   const handleSearchSubmit = (e: FormEvent) => {
     e.preventDefault();
-    if (searchText.length < 3) {
-      setInvalidSearch(true);
-    } else {
-      setInvalidSearch(false);
+    validateSearch(searchText);
+    if (!invalidSearch) {
       setSearchTitle(searchText);
       setSearchResult(undefined);
       search();
@@ -47,7 +50,11 @@ export const MovieComponent: React.FC = () => {
             placeholder="Enter search text"
             type="text"
             value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
+            onChange={(e) => {
+              const value = e.target.value;
+              setSearchText(value);
+              validateSearch(value);
+            }}
             onClear={() => setSearchText("")}
           />
         </div>

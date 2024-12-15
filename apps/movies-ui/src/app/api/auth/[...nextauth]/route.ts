d16fb482bc8519798e3caf997c3394871d7a3a03
viper3400 <viper_3400@gmx.de>
2024-12-15 18:37:37 +0100

    Enable oauth via Google

--- apps/movies-ui/src/app/api/auth/[...nextauth]/route.ts
+++ apps/movies-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,19 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const githubId = process.env.GITHUB_ID;
 const githubSecret = process.env.GITHUB_SECRET;
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
 
 if (!githubId || !githubSecret) {
   throw new Error("GITHUB_ID and GITHUB_SECRET must be set in the environment variables.");
+}
+
+if (!googleClientId || !googleClientSecret) {
+  throw new Error("GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set in the environment variables.");
 }
 
 const handler = NextAuth({
@@ -14,6 +22,10 @@ const handler = NextAuth({
       clientId: githubId,
       clientSecret: githubSecret,
     }),
+    GoogleProvider({
+      clientId: googleClientId,
+      clientSecret: googleClientSecret
+    })
   ],
   pages: {
     error: '/auth/error', // Error code passed in query string as ?error=

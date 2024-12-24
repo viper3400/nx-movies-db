3978469eaf75681493027d9c10b9fff801cf997d
viper3400 <viper_3400@gmx.de>
2024-12-24 10:29:45 +0100

    (graphq-lib) Implement seen dates

---
+++ libs/movies-graphql-lib/src/lib/graphql/types/seen-dates.ts
@@ -0,0 +1,22 @@
+import { builder } from "../builder";
+import {getSeenDates, VideoSeenDateArgs } from '@nx-movies-db/movies-prisma-lib';
+
+builder.prismaObject("homewebbridge_userseen", {
+  fields: (t: any) => ({
+    username: t.exposeString("asp_username"),
+    viewdate: t.exposeString("viewdate"),
+  }),
+});
+
+builder.queryField("seenData", (t) =>
+  t.prismaField({
+    type: ["homewebbridge_userseen"],
+    args: {
+      movieId: t.arg.int(),
+      viewGroup: t.arg.string(),
+    },
+    resolve: async (query, _parent, args, _ctx: any, _info) => {
+      return await getSeenDates( args as VideoSeenDateArgs, query);
+    },
+  }),
+);

248c9ecea71ab06888635a8627ef422d972a7f29
viper3400 <viper_3400@gmx.de>
2025-02-22 10:10:18 +0100

    chore(eslint): remove old ESLint configurations and add new flat config structure

---
+++ eslint.config.mjs
@@ -0,0 +1,92 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import nxEslintPlugin from '@nx/eslint-plugin';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    plugins: {
+      '@nx': nxEslintPlugin,
+      'unused-imports': eslintPluginUnusedImports,
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      indent: ['error', 2],
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  ...compat
+    .config({
+      extends: ['plugin:@nx/typescript'],
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+      rules: {
+        ...config.rules,
+        semi: ['error', 'always'],
+        quotes: ['error', 'double'],
+        indent: ['error', 2],
+        'unused-imports/no-unused-imports': 'error',
+      },
+    })),
+  ...compat
+    .config({
+      extends: ['plugin:@nx/javascript'],
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+      rules: {
+        ...config.rules,
+        semi: ['error', 'always'],
+        quotes: ['error', 'double'],
+        indent: ['error', 2],
+        'unused-imports/no-unused-imports': 'error',
+      },
+    })),
+  ...compat
+    .config({
+      env: {
+        jest: true,
+      },
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+      rules: {
+        ...config.rules,
+        semi: ['error', 'always'],
+        quotes: ['error', 'double'],
+        indent: ['error', 2],
+        'unused-imports/no-unused-imports': 'error',
+      },
+    })),
+];

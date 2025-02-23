4ba28f7a875dc5c349afafa43b9210ce5db217e8
viper3400 <viper_3400@gmx.de>
2025-02-23 15:51:22 +0100

    feat(movies): enhance movie search with favorite and watch again filters

--- apps/movies-ui/src/app/services/actions/getMovies.ts
+++ apps/movies-ui/src/app/services/actions/getMovies.ts
@@ -6,8 +6,27 @@ import { getClient } from "../../../lib/apollocient";
 
 // GraphQL query
 const getMovieByTitle = gql`
-  query GetMovies($title: String!, $diskid: String!, $deleteMode: DeleteMode, $take: Int!, $skip: Int!) {
-    videos(title: $title, diskid: $diskid, deleteMode: $deleteMode, queryPlot: true, take: $take, skip: $skip) {
+  query GetMovies(
+    $title: String!,
+    $diskid: String!,
+    $deleteMode: DeleteMode,
+    $filterFavorites: Boolean!,
+    $filterFlagged: Boolean!,
+    $userName: String!,
+    $take: Int!,
+    $skip: Int!)
+    {
+    videos(
+      title: $title,
+      diskid: $diskid,
+      deleteMode: $deleteMode,
+      filterFavorites: $filterFavorites,
+      filterFlagged: $filterFlagged,
+      userName: $userName,
+      queryPlot: true,
+      take: $take,
+      skip: $skip)
+      {
       videos {
         title
         diskid
@@ -25,7 +44,15 @@ const getMovieByTitle = gql`
   }
 `;
 
-export async function getMovies(searchString: string, deleteMode: string, take: number, skip: number) {
+export async function getMovies(
+  searchString: string,
+  deleteMode: string,
+  filterFavorites: boolean,
+  filterFlagged: boolean,
+  userName: string,
+  take: number,
+  skip: number) {
+
   let searchTitle = "";
   let searchDiskId = "";
   const diskidRegex = /^R\d{2}F\d/;
@@ -37,7 +64,16 @@ export async function getMovies(searchString: string, deleteMode: string, take: 
 
   const { data } = await getClient().query({
     query: getMovieByTitle,
-    variables: { title: searchTitle, diskid: searchDiskId, deleteMode: deleteMode, take: take, skip: skip },
+    variables: {
+      title: searchTitle,
+      diskid: searchDiskId,
+      deleteMode: deleteMode,
+      filterFavorites: filterFavorites,
+      filterFlagged: filterFlagged,
+      userName: userName,
+      take: take,
+      skip: skip
+    },
   });
 
   const result = await data;

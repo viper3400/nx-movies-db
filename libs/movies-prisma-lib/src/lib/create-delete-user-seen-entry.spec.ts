69bd0f915c7983da3b539ddc18ddb038e6215ea0
viper3400 <viper_3400@gmx.de>
2025-02-26 19:41:15 +0100

    feat(user-seen): implement create and delete user seen entry functionality with tests

---
+++ libs/movies-prisma-lib/src/lib/create-delete-user-seen-entry.spec.ts
@@ -0,0 +1,73 @@
+import { createUserSeenEntry, deleteUserSeenEntry, getSeenDates } from ".";
+
+describe("create seen date", () => {
+  it("should create & delete seen date for a user in a viewgroup at a given date", async () => {
+    const userName = "user.seen";
+    const viewGroup = "group_a";
+    const movieId = 50;
+    const viewDate = new Date(Date.UTC(2022, 10, 5, 0, 0, 0));
+
+    // create entry
+    const createResult = await createUserSeenEntry({ movieId, userName, viewGroup, viewDate });
+    expect(createResult).toMatchObject({
+      vdb_videoid: movieId,
+      asp_viewgroup: viewGroup,
+      asp_username: userName,
+      viewdate: viewDate
+    });
+
+    // check that entry exists
+    let seenDate = await getSeenDates({ movieId, viewGroup }, undefined);
+    expect(seenDate.length).toBe(1);
+    // delete entry
+    const deleted = await deleteUserSeenEntry({ movieId, viewGroup, viewDate });
+    // check that entry does not exist
+    seenDate = await getSeenDates({ movieId, viewGroup }, undefined);
+    expect(seenDate.length).toBe(0);
+
+  });
+
+  it("should not allow to create seen date twice a day for a viewgroup", async () => {
+    const userNameA = "user.seenA";
+    const userNameB = "user.seenB";
+    const viewGroup = "group_b";
+    const movieId = 52;
+    const viewDate = new Date(Date.UTC(2022, 10, 5, 0, 0, 0));
+
+    // create entry 1
+    const createResultOne = await createUserSeenEntry({
+      movieId: movieId,
+      userName: userNameA,
+      viewGroup: viewGroup,
+      viewDate: viewDate
+    });
+
+    expect(createResultOne).toMatchObject({
+      vdb_videoid: movieId,
+      asp_viewgroup: viewGroup,
+      asp_username: userNameA,
+      viewdate: viewDate
+    });
+
+    // create entry 2
+    expect(createUserSeenEntry({
+      movieId: movieId,
+      userName: userNameA,
+      viewGroup: viewGroup,
+      viewDate: viewDate
+    })).rejects.toThrow();
+
+
+    // check that entry exists
+    let seenDate = await getSeenDates({ movieId, viewGroup }, undefined);
+    expect(seenDate.length).toBe(1);
+    // delete entry
+    const deleted = await deleteUserSeenEntry({ movieId, viewGroup, viewDate });
+    console.log(deleted);
+    // check that entry does not exist
+    seenDate = await getSeenDates({ movieId, viewGroup }, undefined);
+
+    expect(seenDate.length).toBe(0);
+
+  });
+});

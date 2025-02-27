e4b59489f0f9785248bf2b952237e7ffefc72c84
viper3400 <viper_3400@gmx.de>
2025-02-27 16:23:05 +0100

    feat(user-seen): add create and delete user seen entry mutations with date handling

--- libs/movies-graphql-lib/src/lib/graphql/types/seen-dates.ts
+++ libs/movies-graphql-lib/src/lib/graphql/types/seen-dates.ts
@@ -1,5 +1,5 @@
 import { builder } from "../builder";
-import {getSeenDates, VideoSeenDateArgs } from "@nx-movies-db/movies-prisma-lib";
+import { createUserSeenEntry, deleteUserSeenEntry, getSeenDates, VideoSeenDateArgs } from "@nx-movies-db/movies-prisma-lib";
 
 builder.prismaObject("homewebbridge_userseen", {
   fields: (t: any) => ({
@@ -21,7 +21,49 @@ builder.queryField("seenData", (t) =>
       viewGroup: t.arg.string(),
     },
     resolve: async (query, _parent, args, _ctx: any, _info) => {
-      return await getSeenDates( args as VideoSeenDateArgs, query);
+      return await getSeenDates(args as VideoSeenDateArgs, query);
     },
   }),
+);
+
+builder.mutationField("createUserSeenEntry", (t) =>
+  t.prismaField({
+    type: "homewebbridge_userseen",
+    args: {
+      movieId: t.arg.int({ required: true }),
+      viewGroup: t.arg.string({ required: true }),
+      viewDate: t.arg.string({ required: true }),
+      userName: t.arg.string({ required: true })
+    },
+    resolve: async (query, root, args, ctx) => {
+      const date = Date.parse(args.viewDate);
+      const result = await createUserSeenEntry({
+        movieId: args.movieId,
+        viewGroup: args.viewGroup,
+        viewDate: new Date(date),
+        userName: args.userName
+      });
+      return result;
+    },
+  })
+);
+
+builder.mutationField("deleteUserSeenEntry", (t) =>
+  t.prismaField({
+    type: "homewebbridge_userseen",
+    args: {
+      movieId: t.arg.int({ required: true }),
+      viewGroup: t.arg.string({ required: true }),
+      viewDate: t.arg.string({ required: true }),
+    },
+    resolve: async (query, root, args, ctx) => {
+      const date = Date.parse(args.viewDate);
+      await deleteUserSeenEntry({
+        movieId: args.movieId,
+        viewGroup: args.viewGroup,
+        viewDate: new Date(date),
+      });
+      return null;
+    },
+  })
 );

a7248687ce0fba529b04cbc8a95b4d46095393a1
viper3400 <viper_3400@gmx.de>
2025-05-06 15:47:24 +0200

    feat: pass userName prop to SeenMoviesComponent for personalized movie display

--- apps/movies-ui/src/components/seenMovies.tsx
+++ apps/movies-ui/src/components/seenMovies.tsx
@@ -1,11 +1,43 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getSeenVideos } from "../app/services/actions";
-import { SeenVideosQueryResult } from "../interfaces";
+import { deleteUserSeenDate, getSeenDates, getSeenVideos, updateUserFlags } from "../app/services/actions";
+import { SeenVideosQueryResult, UserFlagsDTO } from "../interfaces";
+import { MovieCard } from "@nx-movies-db/shared-ui";
+import { getUserFlagsForMovie } from "../app/services/actions/getUserFlags";
+import { getAppBasePath } from "../app/services/actions/getAppBasePath";
 
-export const SeenMoviesComponent = () => {
+interface SeenMoviesComponentProperties {
+  userName: string
+}
+export const SeenMoviesComponent = ({ userName }: SeenMoviesComponentProperties) => {
   const [seenMovies, setSeenMovies] = useState<SeenVideosQueryResult>();
+  const [imageBaseUrl, setImageBaseUrl] = useState<string>();
+
+  const loadUserFlagsForMovie = async (movieId: string) => {
+    const flags = await getUserFlagsForMovie(movieId, userName);
+    return flags;
+  };
+
+  const loadSeenDatesForMovie = async (movieId: string) => {
+    const seenDates = await getSeenDates(movieId, "VG_Default");
+    return seenDates;
+  };
+
+  const updateUserFlagsForMovie = async (flags: UserFlagsDTO) => {
+    await updateUserFlags(
+      parseInt(flags.movieId),
+      flags.isFavorite,
+      flags.isWatchAgain,
+      userName);
+  };
+
+  const deleteUserSeenDateForMovie = async (movieId: string, date: Date) => {
+    await deleteUserSeenDate(
+      parseInt(movieId),
+      date.toISOString().slice(0, 10),
+      "VG_Default");
+  };
 
   useEffect(() => {
     const fetchSeenMovies = async () => {
@@ -19,13 +51,37 @@ export const SeenMoviesComponent = () => {
       setSeenMovies(movies);
     };
 
+    const fetchAppBasePath = async () => {
+      const appBasePath = await getAppBasePath();
+      setImageBaseUrl(appBasePath + "/api/cover-image");
+    };
+
+    fetchAppBasePath();
+
     fetchSeenMovies();
   }, []);
 
   return (
     <div>
-      {seenMovies ? (
-        seenMovies.seenVideos.SeenEntries.map((entry) => (<p key={entry.movieId}>{entry.video.title}</p>))
+      {seenMovies && imageBaseUrl ? (
+        seenMovies.seenVideos.SeenEntries.map((entry) => (
+          <MovieCard
+            key={entry.movieId} movie={entry.video}
+            showMarkAsSeenButtons={false}
+            showDetailsButton
+            imageUrl={imageBaseUrl + "/" + entry.movieId}
+            loadSeenDatesForMovie={loadSeenDatesForMovie}
+            loadUserFlagsForMovie={loadUserFlagsForMovie}
+            updateFlagsForMovie={updateUserFlagsForMovie}
+            setUserSeenDateForMovie={function (movieId: string, date: Date): Promise<void> {
+              throw new Error("Function not implemented.");
+            }} deleteUserSeenDateForMovie={deleteUserSeenDateForMovie}
+            langResources={{
+              "seenTodayLabel": "Seen Today",
+              "chooseDateLabel": "Choose Date",
+              "deletedEntryLabel": "Deleted Entry"
+            }} />
+        ))
       ) : (
         <p>Loading...</p>
       )}

7ff7821fda360f3a6b764f8a1f4ebe8e57e7c327
viper3400 <viper_3400@gmx.de>
2025-05-07 09:40:06 +0200

    feat: refactor NavbarComponent to use Link instead of Button for navigation and logout actions

--- apps/movies-ui/src/components/navbar.tsx
+++ apps/movies-ui/src/components/navbar.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { Navbar, NavbarBrand, NavbarContent, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@heroui/navbar";
-import { Button, Link, Spacer, User } from "@heroui/react";
+import { Divider, Link, Spacer, User } from "@heroui/react";
 import { ThemeSwitch } from "./theme-switch";
 import { SceneLogo } from "../icons/icons";
 import { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 export default function NavbarComponent() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const router = useRouter();
   const { data: session } = useSession();
 
   return (
@@ -39,31 +37,52 @@ export default function NavbarComponent() {
         {
           session &&
           <>
-            <NavbarMenuItem>
-              <Link onPress={() => {
-                router.push("/");
-                setIsMenuOpen(false);
-              }}>Filmsuche</Link>
-            </NavbarMenuItem>
+            <Divider orientation="horizontal" />
             <NavbarMenuItem>
               <Link
+                href="/"
                 onPress={() => {
-                  router.push("/seen");
+                  setIsMenuOpen(false);
+                }}>Filmsuche</Link>
+            </NavbarMenuItem>
+            <Divider orientation="horizontal" />
+            <NavbarMenuItem>
+              <Link
+                href="/seen"
+                onPress={() => {
                   setIsMenuOpen(false);
                 }}>Gesehene Filme</Link>
             </NavbarMenuItem>
+            <Divider orientation="horizontal" />
             <NavbarMenuItem>
-              <Button onPress={() => signOut()}>HomeWeb Logout</Button>
+              <Link
+                href=""
+                onPress={() => {
+                  signOut();
+                  setIsMenuOpen(false);
+                }}>HomeWeb Logout</Link>
             </NavbarMenuItem>
             {session?.user?.email?.match(/@(gmail\.com|.*\.google\.com)$/) && (
-              <NavbarMenuItem>
-                <Button onPress={() => { signOut(); window.open("https://accounts.google.com/Logout", "_blank"); }}>Google Logout</Button>
-              </NavbarMenuItem>
+              <>
+                <Divider orientation="horizontal" />
+                <NavbarMenuItem>
+                  <Link
+                    href=""
+                    isExternal
+                    onPress={() => { signOut(); window.open("https://accounts.google.com/Logout"); }}>Google Logout</Link>
+                </NavbarMenuItem>
+              </>
             )}
             {session?.user?.email?.includes("@github.com") && (
-              <NavbarMenuItem>
-                <Button onPress={() => { signOut(); window.open("https://github.com/logout", "_blank"); }}>Github Logout</Button>
-              </NavbarMenuItem>
+              <>
+                <Divider orientation="horizontal" />
+                <NavbarMenuItem>
+                  <Link
+                    href=""
+                    isExternal
+                    onPress={() => { signOut(); window.open("https://github.com/logout",); }}>Github Logout</Link>
+                </NavbarMenuItem>
+              </>
             )}
           </>
         }

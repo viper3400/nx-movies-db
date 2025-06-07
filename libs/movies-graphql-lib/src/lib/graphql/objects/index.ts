ca2b3eb1c7e19e346396d5a279645c1d5c129215
viper3400 <viper_3400@gmx.de>
2025-06-07 16:11:52 +0200

    feat: add genres functionality with Prisma integration and GraphQL queries

--- libs/movies-graphql-lib/src/lib/graphql/objects/index.ts
+++ libs/movies-graphql-lib/src/lib/graphql/objects/index.ts
@@ -3,5 +3,6 @@ export * from "./request-meta";
 export * from "./seen-entry";
 export * from "./tv-series-mode";
 export * from "./video";
+export * from "./videodb-genres";
 export * from "./videodb-mediatypes";
 export * from "./videos";

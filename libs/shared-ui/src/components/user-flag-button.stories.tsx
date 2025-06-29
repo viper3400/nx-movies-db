55bba01f6f521cc0ed4b989cda9958f5fe71c4a2
viper3400 <viper_3400@gmx.de>
2025-06-29 18:13:51 +0200

    chore: update Storybook and related dependencies to latest versions
    - Upgraded @storybook/addon-styling-webpack from 1.0.1 to 2.0.0
    - Upgraded @chromatic-com/storybook from 3.2.6 to 4.0.1
    - Updated Storybook packages to version 9.0.14
    - Added eslint-plugin-storybook and @storybook/nextjs-vite at version 9.0.14
    - Added @storybook/addon-docs at version 9.0.14

--- libs/shared-ui/src/components/user-flag-button.stories.tsx
+++ libs/shared-ui/src/components/user-flag-button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { UserFlagButton } from "./user-flag-button";
 
 const meta: Meta<typeof UserFlagButton> = {

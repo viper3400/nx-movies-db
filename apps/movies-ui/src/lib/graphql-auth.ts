2b177bee19a2160d8501c4d2d7c3461f37e376e8
viper3400 <viper_3400@gmx.de>
2025-10-26 11:57:16 +0100

    (chore) fixed linting issues

--- apps/movies-ui/src/lib/graphql-auth.ts
+++ apps/movies-ui/src/lib/graphql-auth.ts
@@ -4,7 +4,7 @@ import { StringValue } from "ms";
 export async function signApiToken({ sub, email, name, expiresIn = "15Mins" }:
   { sub: string | undefined, email: string | undefined, name: string | undefined, expiresIn?: StringValue | number }) {
 
-  console.log("Create a short-lived API token (Bearer) for GraphQL")
+  console.log("Create a short-lived API token (Bearer) for GraphQL");
   // Create a short-lived API token (Bearer) for GraphQL
   const token = jwt.sign(
     {

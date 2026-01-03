98f2d9d94c6fdce052151bcf5c08cbbf1ae36388
viper3400 <viper_3400@gmx.de>
2026-01-03 17:51:58 +0100

    (ci) additional jest logs

--- libs/movies-prisma-lib/jest.setup.ts
+++ libs/movies-prisma-lib/jest.setup.ts
@@ -1,5 +1,6 @@
 // libs/movies-prisma-lib/jest.setup.ts
 import * as dotenv from "dotenv";
+import { prisma } from "./src/prismaclient";
 
 dotenv.config({
   path: !process.env.CI && process.env.NODE_ENV !== "production"
@@ -12,4 +13,15 @@ console.log("[JEST ENV]", {
   DATABASE_HOST: process.env.DATABASE_HOST,
   DATABASE_PORT: process.env.DATABASE_PORT,
   DATABASE_USER: process.env.DATABASE_USER
+});
+
+beforeAll(async () => {
+  console.log("[JEST ENV] Connecting to database...");
+  await prisma.$connect();
+  await prisma.$queryRaw`SELECT 1`;
+  console.log("[JEST ENV] Database connection established.");
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });

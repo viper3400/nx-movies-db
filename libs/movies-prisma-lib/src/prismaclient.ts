160d3de764a84dce68f846d75c0cb25e6d7e4c84
viper3400 <viper_3400@gmx.de>
2026-01-03 17:44:36 +0100

    (ci) added prisma client logging

--- libs/movies-prisma-lib/src/prismaclient.ts
+++ libs/movies-prisma-lib/src/prismaclient.ts
@@ -1,11 +1,28 @@
 import { PrismaClient } from "./generated/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const adapter = new PrismaMariaDb({
+const connectionConfig = {
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT || "3306", 10)
+  port: parseInt(process.env.DATABASE_PORT || "3306", 10),
+};
+
+if (!connectionConfig.host) {
+  throw new Error("DATABASE_HOST is not defined – Prisma client cannot start.");
+}
+
+console.log("[PRISMA CLIENT]", {
+  ...connectionConfig,
+  password: connectionConfig.password ? "***" : undefined,
 });
+
+const adapter = new PrismaMariaDb(connectionConfig, {
+  database: connectionConfig.database,
+  onConnectionError: (err) => {
+    console.error("[PRISMA CLIENT] Connection error", err);
+  }
+});
+
 export const prisma = new PrismaClient({ adapter });

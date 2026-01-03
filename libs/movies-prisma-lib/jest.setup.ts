7ce9f8d75813048a94caa885afb287c06a3883dc
viper3400 <viper_3400@gmx.de>
2026-01-03 14:46:38 +0100

    (lint) fixed linting issues

--- libs/movies-prisma-lib/jest.setup.ts
+++ libs/movies-prisma-lib/jest.setup.ts
@@ -1,13 +1,13 @@
 // libs/movies-prisma-lib/jest.setup.ts
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config({
-  path: !process.env.CI && process.env.NODE_ENV !== 'production'
-    ? '.env.local'
-    : '.env',
+  path: !process.env.CI && process.env.NODE_ENV !== "production"
+    ? ".env.local"
+    : ".env",
 });
 
-console.log('[JEST ENV]', {
+console.log("[JEST ENV]", {
   DATABASE_URL: process.env.DATABASE_URL,
   DATABASE_HOST: process.env.DATABASE_HOST,
   DATABASE_PORT: process.env.DATABASE_PORT,

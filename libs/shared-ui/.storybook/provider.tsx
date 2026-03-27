6ec36af849007f4bc30bd80583f84285620ef79a
viper3400 <viper_3400@gmx.de>
2026-03-27 10:50:26 +0100

    fixed next-themes in storybook

--- libs/shared-ui/.storybook/provider.tsx
+++ libs/shared-ui/.storybook/provider.tsx
@@ -2,23 +2,20 @@
 
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/react";
-import {
-  ThemeProvider as NextThemesProvider,
-  ThemeProviderProps,
-} from "next-themes";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../src/i18n/i18n";
+import { ThemeProvider as SharedThemeProvider, ThemeProviderOptions } from "../src/lib/theme-provider";
 
 export interface ProvidersProps {
   children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
+  themeProps?: ThemeProviderOptions;
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <I18nextProvider i18n={i18n}>
       <HeroUIProvider>
-        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        <SharedThemeProvider {...themeProps}>{children}</SharedThemeProvider>
       </HeroUIProvider>
     </I18nextProvider>
   );

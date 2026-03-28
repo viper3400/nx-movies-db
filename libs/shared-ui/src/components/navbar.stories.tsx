44b4ab79e669b811cfed6ac520dc118c50062142
viper3400 <viper_3400@gmx.de>
2026-03-28 10:41:53 +0100

    (feature) app version field in navbar

--- libs/shared-ui/src/components/navbar.stories.tsx
+++ libs/shared-ui/src/components/navbar.stories.tsx
@@ -14,6 +14,7 @@ export const Default: Story = {
     isValidSession: true,
     userEmail: "hans@gmail.com",
     userName: "Hans Dampf",
-    userImage: "https://i.pravatar.cc/150?u=a04258a2462d826712d"
+    userImage: "https://i.pravatar.cc/150?u=a04258a2462d826712d",
+    appVersion: "1.2.3",
   },
 };

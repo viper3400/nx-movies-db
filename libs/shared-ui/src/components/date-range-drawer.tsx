79dd46a591b1982c625a32de3e72877a98cd085e
viper3400 <viper_3400@gmx.de>
2026-06-18 10:37:22 +0200

    fixed linting issues

--- libs/shared-ui/src/components/date-range-drawer.tsx
+++ libs/shared-ui/src/components/date-range-drawer.tsx
@@ -30,10 +30,10 @@ export const DateRangeDrawerComponent = ({ onApply }: DateRangeDrawerComponentPr
   const formatDate = (date: DateValue | null) => {
     return date
       ? new Intl.DateTimeFormat("de-DE", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-        }).format(new Date(date.toString()))
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      }).format(new Date(date.toString()))
       : "";
   };
 
